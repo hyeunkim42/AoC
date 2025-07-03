e3e0e6b7e0596602defc198f3ead12e08de8d96f
hyeunkim <hyeunkim@student.42seoul.kr>
2025-07-03 16:26:26 +0900

    feat: part2 of day3 from 2018 AoC

--- 2018/day03.test.ts
+++ 2018/day03.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "bun:test";
 import * as fs from "node:fs/promises";
-import { day3_part1 } from "./day03";
+import { day3_part1, day3_part2 } from "./day03";
 
 test("day3 part1 test", async() => {
 	expect(day3_part1("#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2")).toBe(4);
@@ -10,4 +10,14 @@ test("day3 par1 real", async() => {
 	const input = await fs.readFile("inputs/day03.txt", "utf-8");
 
 	expect(day3_part1(input)).toBe(116491);
+})
+
+test("day3 part2 test", async() => {
+	expect(day3_part2("#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2")).toBe(3);
+})
+
+test("day3 par2 real", async() => {
+	const input = await fs.readFile("inputs/day03.txt", "utf-8");
+
+	expect(day3_part2(input)).toBe(707);
 })
--- 2018/day03.ts
+++ 2018/day03.ts
@@ -1,3 +1,22 @@
+type fabricArea = {
+	id: number,
+	startX: number,
+	startY: number,
+	width: number,
+	height: number
+}
+
+function parseSquare(input: string): fabricArea {
+	let fabricArea: fabricArea = {
+		id: parseInt(input.slice(input.indexOf("#") + 1, input.indexOf("@"))),
+		startX: parseInt(input.slice(input.indexOf("@") + 1, input.indexOf(","))),
+		startY: parseInt(input.slice(input.indexOf(",") + 1, input.indexOf(":"))),
+		width: parseInt(input.slice(input.indexOf(":") + 1, input.indexOf("x"))),
+		height: parseInt(input.slice(input.indexOf("x") + 1))
+	}
+	return fabricArea;
+}
+
 export function day3_part1(input: string) {
 	const data = input.split("\n");
 
@@ -5,14 +24,11 @@ export function day3_part1(input: string) {
 	let commonArea: number = 0;
 
 	for (let line of data) {
-		const startX = parseInt(line.slice(line.indexOf("@") + 1, line.indexOf(",")));
-		const startY = parseInt(line.slice(line.indexOf(",") + 1, line.indexOf(":")));
-		const width = parseInt(line.slice(line.indexOf(":") + 1, line.indexOf("x")));
-		const height = parseInt(line.slice(line.indexOf("x") + 1));
+		const fabricArea = parseSquare(line);
 
-		for (let dx = 0; dx < width; dx++) {
-			for (let dy = 0; dy < height; dy++) {
-				const currCoord: string = `${startX + dx},${startY + dy}`;
+		for (let dx = 0; dx < fabricArea.width; dx++) {
+			for (let dy = 0; dy < fabricArea.height; dy++) {
+				const currCoord: string = `${fabricArea.startX + dx},${fabricArea.startY + dy}`;
 				let count: number = area.get(currCoord) || 0;
 
 				if (count == 1) {
@@ -23,4 +39,38 @@ export function day3_part1(input: string) {
 		}
 	}
 	return commonArea;
+}
+
+export function day3_part2(input: string): number {
+	const data: string[] = input.split("\n");
+
+	let area: Map<string, number> = new Map();
+	for (const line of data) {
+		const fabricArea = parseSquare(line);
+
+		for (let dx = 0; dx < fabricArea.width; dx++) {
+			for (let dy = 0; dy < fabricArea.height; dy++) {
+				const currCoord = `${fabricArea.startX + dx},${fabricArea.startY + dy}`;
+				let count: number = area.get(currCoord) || 0;
+
+				area.set(currCoord, count + 1);
+			}
+		}
+	}
+	for (const line of data) {
+		const fabricArea = parseSquare(line);
+
+		let isSeparate = true;
+		for (let dx = 0; dx < fabricArea.width; dx++) {
+			for (let dy = 0; dy < fabricArea.height; dy++) {
+				const currCoord = `${fabricArea.startX + dx},${fabricArea.startY + dy}`;
+				const claim : number = area.get(currCoord) || 0;
+				if (claim > 1)
+					isSeparate = false;
+			}
+		}
+		if (isSeparate)
+			return fabricArea.id;
+	}
+	return 0;
 }
