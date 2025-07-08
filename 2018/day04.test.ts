0d34f665e007f59fffc411c5b3251b641eff3780
hyeunkim <hyeunkim@student.42seoul.kr>
2025-07-08 15:10:29 +0900

    feat: part2 of day4 from 2018 AoC & fix: part1 of day4

--- 2018/day04.test.ts
+++ 2018/day04.test.ts
@@ -12,12 +12,12 @@ test("day4 par1 real", async() => {
 	expect(day4_part1(input)).toBe(106710);
 })
 
-// test("day4 part2 test", async() => {
-// 	expect(day4_part2("[1518-11-01 00:05] falls asleep\n[1518-11-01 00:25] wakes up\n[1518-11-01 00:30] falls asleep\n[1518-11-01 00:55] wakes up\n[1518-11-01 23:58] Guard #99 begins shift\n[1518-11-02 00:40] falls asleep\n[1518-11-02 00:50] wakes up\n[1518-11-03 00:05] Guard #10 begins shift\n[1518-11-03 00:24] falls asleep\n[1518-11-03 00:29] wakes up\n[1518-11-01 00:00] Guard #10 begins shift\n[1518-11-04 00:02] Guard #99 begins shift\n[1518-11-04 00:36] falls asleep\n[1518-11-04 00:46] wakes up\n[1518-11-05 00:03] Guard #99 begins shift\n[1518-11-05 00:45] falls asleep\n[1518-11-05 00:55] wakes up")).toBe(4455);
-// })
+test("day4 part2 test", async() => {
+	expect(day4_part2("[1518-11-01 00:05] falls asleep\n[1518-11-01 00:25] wakes up\n[1518-11-01 00:30] falls asleep\n[1518-11-01 00:55] wakes up\n[1518-11-01 23:58] Guard #99 begins shift\n[1518-11-02 00:40] falls asleep\n[1518-11-02 00:50] wakes up\n[1518-11-03 00:05] Guard #10 begins shift\n[1518-11-03 00:24] falls asleep\n[1518-11-03 00:29] wakes up\n[1518-11-01 00:00] Guard #10 begins shift\n[1518-11-04 00:02] Guard #99 begins shift\n[1518-11-04 00:36] falls asleep\n[1518-11-04 00:46] wakes up\n[1518-11-05 00:03] Guard #99 begins shift\n[1518-11-05 00:45] falls asleep\n[1518-11-05 00:55] wakes up")).toBe(4455);
+})
 
-// test("day4 par2 real", async() => {
-// 	const input = await fs.readFile("inputs/day04.txt", "utf-8");
+test("day4 part2 real", async() => {
+	const input = await fs.readFile("inputs/day04.txt", "utf-8");
 
-// 	expect(day4_part2(input)).toBe(707);
-// })
+	expect(day4_part2(input)).toBe(707);
+})
--- 2018/day04.ts
+++ 2018/day04.ts
@@ -56,21 +56,49 @@ export function day4_part1(input: string) {
 	let sleeplength: number = 0;
 	let mostCommon: number = 0;
 	let repTime: number = 0;
-	guards.forEach((value: GuardInfo, id:number) => {
-		if (value.accLen > sleeplength) {
+	guards.forEach((guard: GuardInfo, id: number) => {
+		if (guard.accLen > sleeplength) {
 			mostSlept = id;
-			sleeplength = value.accLen;
-			for (let i = 0; i < 60; i++) {
-				if (value.sleepMap.get(i) || 0 > repTime){
-					repTime = value.sleepMap.get(i) || 0;
-					mostCommon = i;
+			sleeplength = guard.accLen;
+			guard.sleepMap.forEach((count: number, minute: number)=> {
+				if (repTime < count) {
+					repTime = count;
+					mostCommon = minute;
 				}
-			}
+			})
 		}
 	})
 	return mostSlept * mostCommon ;
 }
 
 export function day4_part2(input: string) {
-	return 0;
+		const data = input.split("\n").sort();
+
+	let guards: Map<number, GuardInfo> = new Map();
+	let guardID: number = 0;
+	let start: number = 0;
+
+	for (const line of data) {
+		if (line.indexOf("#") > 0) {
+			guardID = addNewGuard(guards, parseInt(line.slice(line.indexOf("#") + 1)));
+		} else if (line.indexOf("fall") > 0){
+			start = parseInt(line.slice(line.indexOf(":") + 1));
+		} else {
+			recordSleep(guards, guardID, start, parseInt(line.slice(line.indexOf(":") + 1)));
+		}
+	}
+	// find id of most common minute and id
+	let mostSlept: number = 0;
+	let mostCommon: number = 0;
+	let repTime: number = 0;
+	guards.forEach((guard: GuardInfo, id:number) => {
+		guard.sleepMap.forEach((count: number, minute: number) => {
+			if (count > repTime) {
+				repTime = count;
+				mostCommon = minute;
+				mostSlept = id;
+			}
+		})
+	})
+	return mostSlept * mostCommon ;
 }
