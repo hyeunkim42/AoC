db837b7ccceb7cda7903f550ba9718e69f835e28
hyeunkim <hyeunkim@student.42seoul.kr>
2025-07-09 21:10:17 +0900

    feat: part2 of day5 from 2018 AoC

--- 2018/day05.ts
+++ 2018/day05.ts
@@ -1,4 +1,4 @@
-export function day5_part1(input: string) {
+function reactPolymerMethod1(input: string) {
 	const lowerUpper = Array.from({ length: 26 }, (_, i) => {
 		const lower = String.fromCharCode(97 + i);
 		const upper = String.fromCharCode(65 + i);
@@ -18,22 +18,41 @@ export function day5_part1(input: string) {
 		input = input.replace(regexp, "");
 	} while (prevLength !== input.length)
 
-	return input.length;
-} // 3.42s
+	return input;
+}
 
-export function day5_part1_m2(input:string) {
+function reactPolymerMethod2(input: string) {
 	let stack: string[] = [];
-	for (let c of input) {
+	for (let char of input) {
 		const last = stack.at(stack.length - 1);
-		if (last && last !== c && last.toLowerCase() === c.toLowerCase()) {
+		if (last && last !== char && last.toLowerCase() === char.toLowerCase()) {
 			stack.pop();
 		} else {
-			stack.push(c);
+			stack.push(char);
 		}
 	}
-	return stack.length;
-} //11.47ms
+	return stack.join('');
+}
+
+export function day5_part1(input: string) {
+	// const result1 = reactPolymerMethod1(input); // 3.42s
+	const result2 = reactPolymerMethod2(input); // 11.47ms
+	// console.log(result2);
+	return result2.length;
+}
 
 export function day5_part2(input: string) {
-	return 0;
+	let minLen = input.length;
+	const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (const c of alphabet) {
+		const pattern = c + "|" + c.toLowerCase();
+		const regexp = new RegExp(pattern, "g");
+		const removedstring = input.replace(regexp, "");
+		const fullyReactedString = reactPolymerMethod2(removedstring);
+
+		if (minLen > fullyReactedString.length)
+			minLen = fullyReactedString.length;
+	}
+	return minLen;
 }
