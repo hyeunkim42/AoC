80cf1482be70151268068b5e8789f0f902ff657a
hyeunkim <hyeunkim@student.42seoul.kr>
2025-07-10 15:23:47 +0900

    fix: typo

--- 2018/day04.test.ts
+++ 2018/day04.test.ts
@@ -6,7 +6,7 @@ test("day4 part1 test", async() => {
 	expect(day4_part1("[1518-11-01 00:05] falls asleep\n[1518-11-01 00:25] wakes up\n[1518-11-01 00:30] falls asleep\n[1518-11-01 00:55] wakes up\n[1518-11-01 23:58] Guard #99 begins shift\n[1518-11-02 00:40] falls asleep\n[1518-11-02 00:50] wakes up\n[1518-11-03 00:05] Guard #10 begins shift\n[1518-11-03 00:24] falls asleep\n[1518-11-03 00:29] wakes up\n[1518-11-01 00:00] Guard #10 begins shift\n[1518-11-04 00:02] Guard #99 begins shift\n[1518-11-04 00:36] falls asleep\n[1518-11-04 00:46] wakes up\n[1518-11-05 00:03] Guard #99 begins shift\n[1518-11-05 00:45] falls asleep\n[1518-11-05 00:55] wakes up")).toBe(240);
 })
 
-test("day4 par1 real", async() => {
+test("day4 part1 real", async() => {
 	const input = await fs.readFile("inputs/day04.txt", "utf-8");
 
 	expect(day4_part1(input)).toBe(106710);
